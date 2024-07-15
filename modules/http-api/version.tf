97427b29f733959e27b47f7cc8507d2128f8e1cc
Ace Kloud-Native <fiduh987@gmail.com>
2024-07-15 13:01:14 +0000

    update folder structure

---
+++ modules/http-api/version.tf
@@ -0,0 +1,12 @@
+
+terraform {
+  # Require Terraform Core at exactly version 1.9.1
+  required_version = "1.9.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
