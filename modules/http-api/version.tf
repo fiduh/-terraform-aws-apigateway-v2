ce2e60283c9b4270bac422d621903a4c57211978
Ace Kloud-Native <fiduh987@gmail.com>
2024-10-08 20:47:19 +0000

    Update version.tf

--- modules/http-api/version.tf
+++ modules/http-api/version.tf
@@ -1,11 +1,11 @@
 
 terraform {
   # Require Terraform Core at exactly version 1.9.1
-  required_version = "1.8.3"
+  required_version = "1.9.7"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.70"
     }
   }
 }
